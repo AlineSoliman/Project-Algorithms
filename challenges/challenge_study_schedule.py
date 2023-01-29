71183af84590dd6cf9880ef6149e6966b36823b5
Aline Soliman <alineapreis@hotmail.com>
2023-01-29 09:07:48 -0300

    feat: implementa função study_schedule para buscar número de estudantes estudando no mesmo horário

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time != 0 or target_time == None:
+        return None
+
+    users_online = 0
+
+    for start_period, end_period in permanence_period:
+        if not start_period or not end_period:
+            return None
+        if start_period <= target_time <= end_period:
+            users_online += 1
+
+        return users_online
