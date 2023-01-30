bccb496dbe3c05231a8873be93f17b8c85ed5793
Aline Soliman <alineapreis@hotmail.com>
2023-01-30 07:40:14 -0300

    feat: altera modo de ordenação de bubble para merge_sort de acordo com o modelo do course para ser mais eficaz

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,31 +1,52 @@
-def sorting_strings(string):
-    letters = string.lower()
-    counter = 0
-    modify = True
+def merge_sort(strings, start=0, end=None):
+    if end is None:
+        end = len(strings)
+    if (end - start) > 1:
+        mid = (start + end) // 2
+        merge_sort(strings, start, mid)
+        merge_sort(strings, mid, end)
+        merge(strings, start, mid, end)
 
-    while modify:
-        modify = False
-        for index in range(len(letters) - counter - 1):
-            if letters[index] > letters[index + 1]:
-                letters[index], letters[index + 1] = \
-                    letters[index + 1], letters[index]
-                modify = True
-        counter += 1
-    return letters
+
+def merge(strings, start, mid, end):
+    left = strings[start:mid]
+    right = strings[mid:end]
+
+    left_index, right_index = 0, 0
+
+    for general_index in range(start, end):
+        if left_index >= len(left):
+            strings[general_index] = right[right_index]
+            right_index = right_index + 1
+        elif right_index >= len(right):
+            strings[general_index] = left[left_index]
+            left_index = left_index + 1
+        elif left[left_index] < right[right_index]:
+            strings[general_index] = left[left_index]
+            left_index = left_index + 1
+        else:
+            strings[general_index] = right[right_index]
+            right_index = right_index + 1
 
 
 def is_anagram(first_string, second_string):
+    first_string = [*first_string.lower()]
+    second_string = [*second_string.lower()]
 
-    if first_string == '' or second_string == '':
-        return (first_string, second_string, False)
+    merge_sort(first_string)
+    merge_sort(second_string)
 
-    if len(first_string) != len(second_string):
-        return (first_string, second_string, False)
+    string1 = "".join(first_string)
+    string2 = "".join(second_string)
 
-    string1 = sorting_strings(first_string)
-    string2 = sorting_strings(second_string)
+    if len(string1) != len(string2):
+        return (string1, string2, False)
+
+    if string1 == '' or string2 == '':
+        return (string1, string2, False)
 
     if string1 == string2:
         return (string1, string2, True)
+
     else:
         return (string1, string2, False)
