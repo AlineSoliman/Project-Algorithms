aa705b33c3c741075df646ffd8ef0606afbf657e
Aline Soliman <alineapreis@hotmail.com>
2023-01-31 19:54:56 -0300

    feat: add código que implementa os testes de uma função de criptografia

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,13 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message('message', 5) == 'assem_eg'
+    assert encrypt_message('message', 7) == 'egassem'
+    assert encrypt_message('message', 2) == 'egass_em'
+
+    with pytest.raises(TypeError):
+        encrypt_message(4, 5)
+    with pytest.raises(TypeError):
+        encrypt_message("message", "1")
